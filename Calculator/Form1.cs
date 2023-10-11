795af799956cd828d28164a0255f8ba9181adaca
Run <run2gotv@gmail.com>
2023-10-11 13:58:03 +0200

    WiP Theme Approach

--- Calculator/Form1.cs
+++ Calculator/Form1.cs
@@ -1,4 +1,4 @@
-using Microsoft.Win32;
+﻿using Microsoft.Win32;
 using System.Data;
 namespace Calculator
 {
@@ -10,8 +10,8 @@ namespace Calculator
         {
             try
             {
-                SplitTextLines();
-                textLines[1] = (textLines[0] != string.Empty) ? dt.Compute(textLines[0], "").ToString() : "Yes." ;
+                TextSplit();
+                textLines[1] = (textLines[0] != string.Empty) ? dt.Compute(textLines[0], "").ToString() : "♥";
                 TextUpdate();
                 Clipboard.SetText(textLines[1]);
                 txtCalc.SelectionStart = txtCalc.Lines[0].Length;
@@ -20,12 +20,16 @@ namespace Calculator
             }
             catch (Exception ex) { textLines[1] = "Invalid Operator"; TextUpdate(); HandleError(ex); }
         }
-        private void SplitTextLines()
+        private void TextSplit()
         {
             textLines = txtCalc.Text.Split(new string[] { "\r\n", "\r", "\n" }, StringSplitOptions.None);
             if (textLines.Length == 1) textLines = new string[] { textLines[0], string.Empty };
         }
-        private void TextUpdate() { txtCalc.Text = $"{textLines[0]}{Environment.NewLine}= {textLines[1]}"; }
+        private void TextUpdate()
+        { 
+            if (textLines[1] != string.Empty) txtCalc.Text = $"{textLines[0]}{Environment.NewLine}= {textLines[1]}";
+            else txtCalc.Text = $"{textLines[0]}";
+        }
         private void TextAppend(string textAppend)
         {
             textLines[0] += textAppend;
@@ -34,39 +38,78 @@ namespace Calculator
         }
         private void TextDelete()
         {
-            try
+            if (textLines[0] != string.Empty)
             {
-                if (textLines[0] != string.Empty)
+                try
                 {
-                    SplitTextLines();
+                    TextSplit();
                     textLines[0] = textLines[0].Substring(0, textLines[0].Length - 1);
                     textLines[1] = string.Empty;
                     TextUpdate();
                 }
+                catch (Exception ex) { HandleError(ex); }
             }
-            catch (Exception ex) { HandleError(ex); }
         }
-        private void ColorToggle(Control parentControl)
+        int temp = 0;
+        private void ColorToggle(bool parentCheck, Control parentControl)
         {
-            parentControl.BackColor = ColorEx.Invert(parentControl.BackColor);
-            parentControl.ForeColor = ColorEx.Invert(parentControl.ForeColor);
-
+            if (parentCheck)
+            {
+                parentControl.BackColor = ColorEx.Invert(parentControl.BackColor);
+                parentControl.ForeColor = ColorEx.Invert(parentControl.ForeColor);
+            }
             Button button = parentControl as Button;
             if (button != null) button.FlatAppearance.BorderColor = ColorEx.Invert(button.FlatAppearance.BorderColor);
-            foreach (Control childControl in parentControl.Controls) ColorToggle(childControl);
+            foreach (Control childControl in parentControl.Controls)
+            {
+                ColorToggle(false, childControl);
+                temp++;
+            }
+
+            /*if (parentCheck)
+            {
+                Button button = parentControl as Button;
+                if (button != null) button.FlatAppearance.BorderColor = ColorEx.Invert(button.FlatAppearance.BorderColor);
+                foreach (Control childControl in parentControl.Controls)
+                {
+                    ColorToggle(true, childControl);
+                    temp++;
+                }
+            }*/
+
+            /*
+            parentControl.BackColor = ColorEx.Invert(parentControl.BackColor);
+            parentControl.ForeColor = ColorEx.Invert(parentControl.ForeColor);
+    
+            Button button = parentControl as Button;
+            if (button != null)
+            {
+                button.FlatAppearance.BorderColor = ColorEx.Invert(button.FlatAppearance.BorderColor);
+                // Invert border color for child controls of the button
+                foreach (Control childControl in button.Controls)
+                {
+                    ColorToggle(false, childControl);
+                }
+            }
+
+            if (parentCheck)
+            {
+                foreach (Control childControl in parentControl.Controls)
+                {
+                    ColorToggle(true, childControl);
+                }
+            }
+            */
         }
         private void HandleError(Exception ex) { if (MenuViewDebug.Checked) MessageBox.Show(ex.ToString(), "Error"); }
         public Calculator()
         {
             InitializeComponent();
-            if ((int)Registry.GetValue("HKEY_CURRENT_USER\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Themes\\Personalize", "AppsUseLightTheme", -1) == 0) { ColorToggle(this); MenuEditDarkmode.Checked = true; }
+            if ((int)Registry.GetValue("HKEY_CURRENT_USER\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Themes\\Personalize", "AppsUseLightTheme", -1) == 0) { MenuEditDarkmode.Checked = true; }
+            if (MenuEditDarkmode.Checked) ColorToggle(true, this);
             this.ActiveControl = txtCalc;
             this.KeyPress += new KeyPressEventHandler(Calculator_KeyPress);
             txtCalc.KeyDown += new KeyEventHandler(txtCalc_KeyDown);
-            tableLayout.BackColor = ColorEx.Invert(tableLayout.BackColor);
-            tableLayout.ForeColor = ColorEx.Invert(tableLayout.ForeColor);
-            menuStrip.BackColor = ColorEx.Invert(menuStrip.BackColor);
-            menuStrip.ForeColor = ColorEx.Invert(menuStrip.ForeColor);
         }
         private void txtCalc_KeyDown(object sender, KeyEventArgs e)
         {
@@ -78,28 +121,28 @@ namespace Calculator
             else txtCalc.Text += e.KeyChar;
         }
         private void MenuTopMost_Click(object sender, EventArgs e) { this.TopMost = MenuEditTopmost.Checked = !MenuEditTopmost.Checked; }
-        private void MenuDarkmode_Click(object sender, EventArgs e) { MenuEditDarkmode.Checked = !MenuEditDarkmode.Checked; ColorToggle(this); }
+        private void MenuDarkmode_Click(object sender, EventArgs e) { MenuEditDarkmode.Checked = !MenuEditDarkmode.Checked; ColorToggle(true, this); }
         private void StripMenuAbout_Click(object sender, EventArgs e) { MessageBox.Show("Sample Calculator", "Info"); }
         private void MenuViewSimple_Click(object sender, EventArgs e)
         {
             MenuViewSimple.Checked = true;
             MenuViewAdvanced.Checked = false;
             MenuViewDebug.Checked = false;
-            txtCalc.Text = "Simple Mode";
+            txtCalc.Text = Environment.NewLine + "Simple Mode";
         }
         private void MenuViewAdvanced_Click(object sender, EventArgs e)
         {
             MenuViewSimple.Checked = false;
             MenuViewAdvanced.Checked = true;
             MenuViewDebug.Checked = false;
-            txtCalc.Text = "Advanced Mode";
+            txtCalc.Text = Environment.NewLine + "Advanced Mode";
         }
         private void MenuViewDebug_Click(object sender, EventArgs e)
         {
             MenuViewSimple.Checked = false;
             MenuViewAdvanced.Checked = false;
             MenuViewDebug.Checked = true;
-            txtCalc.Text = "Debug Mode";
+            txtCalc.Text = Environment.NewLine + "Debug Mode";
         }
         private void btCalc_Click(object sender, EventArgs e) { Calc(); }
         private void btClear_Click(object sender, EventArgs e) { txtCalc.Text = textLines[0] = textLines[1] = string.Empty; }
